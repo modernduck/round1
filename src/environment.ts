e09d9d5caf0e0e143b590d5c4229d71e9a2471f4
impopkable <sompop.kulapalanont@gmail.com>
2022-06-21 14:43:57 +0700

    remove unused code

--- src/environment.ts
+++ src/environment.ts
@@ -2,5 +2,4 @@ export class Environment {
   static ETHER_SCAN_API_KEY = process.env.ETHER_SCAN_API_KEY;
   static COIN_GECKO_ETH_PRICE_URI =
     'https://api.coingecko.com/api/v3/coins/ethereum?localization=false&tickers=false&community_data=false&developer_data=false&sparkline=false';
-  static COIN_GECKO_ETH_BALANCE_URI: any;
 }
